97a1c9f1ccf1897169d2cb3db1c5d3a93d2c8d0a
Patrick Crumley <patrick.crumley@gmail.com>
2020-02-10 16:26:05 -0500

    wUpdating plots to have smaller markers

--- gen_scores.py
+++ gen_scores.py
@@ -16,26 +16,32 @@ import base64
 
 data = {}
 
+__big_ms = 6
+__small_ms = 0.9
 # Games is the JSON we are going to save and load with the
 # front end
 json_list = os.listdir('bbg_json/')
 
 # First we load in all of the jsons
+comment_sum = 0
 for fname in tqdm(json_list):
     filter_set = set([])
     with open(os.path.join('bbg_json',fname), 'r') as f:
-        data[fname.split('.')[0]] = json.load(f)
-        data[fname.split('.')[0]]['game_id'] = fname.split('.')[0]
-        comments_cpy = [comment for comment in data[fname.split('.')[0]]['comments']]
-        data[fname.split('.')[0]]['comments'] = []
-        for comment in comments_cpy:
-            if comment['comment'] in filter_set:
-                pass
-            else:
-                data[fname.split('.')[0]]['comments'].append(comment)
-                filter_set.add(comment['comment'])
+        loaded_game = json.load(f)
+        if loaded_game['stats']['averageweight'] > 0:
+            data[fname.split('.')[0]] = loaded_game
+            data[fname.split('.')[0]]['game_id'] = fname.split('.')[0]
+            comments_cpy = [comment for comment in data[fname.split('.')[0]]['comments']]
+            data[fname.split('.')[0]]['comments'] = []
+            for comment in comments_cpy:
+                if comment['comment'] in filter_set:
+                    pass
+                else:
+                    data[fname.split('.')[0]]['comments'].append(comment)
+                    filter_set.add(comment['comment'])
+                    comment_sum += 1
         #max_comments = max(len(data[fname.split('.')[0]]['comments']), max_comments)
-
+print(f'{comment_sum} comments')
 #print([key for key in data['1'].keys()])
 def for_kids(game):
     ''' The first filter. Based solely off of manufactured
@@ -47,23 +53,90 @@ kids_games = list(filter(for_kids, data.values()))
 
 complexity = [g['stats']['averageweight'] for g in data.values()]
 age =  [np.log(2021-float(g['yearpublished'])) for g in data.values()]
+# Let's plot the average weight vs average score,
+
+plt.figure(figsize=(8,5))
+plt.plot([g['stats']['averageweight'] for g in data.values()], [g['stats']['average'] for g in data.values()], '.', markersize=__small_ms, color = '#0097a7')
+#plt.plot([g['stats']['averageweight'] for g in kids_games], [g['stats']['average'] for g in kids_games],'.', color = '#F2D60C')
+
+plt.ylim(2,10)
+plt.xlim(.75,None)
+plt.axis('off')
+plt.savefig('game_ratings.png', transparent=True, dpi=200)
+
+
+clue = next(filter(lambda x: x['name'].lower() == 'clue', data.values()))
+catan = next(filter(lambda x: x['name'].lower() == 'catan', data.values()))
+gloomhaven =  next(filter(lambda x: x['name'].lower() == 'gloomhaven', data.values()))
+guess_who = data['4143']
+for game in [guess_who, clue, catan, gloomhaven]:
+    plt.plot(game['stats']['averageweight'], game['stats']['average'], '*', markersize=40, color='#B31269')
+plt.ylim(2,10)
+plt.xlim(.75,None)
+plt.savefig('game_ratings_with_games.png', transparent=True, dpi=200)
+
+plt.clf()
+plt.plot([g['stats']['averageweight'] for g in data.values()], [g['stats']['average'] for g in data.values()],'.', markersize=__small_ms, color = '#0097a7')
+
+plt.plot([g['stats']['averageweight'] for g in kids_games], [g['stats']['average'] for g in kids_games],'.', markersize=__small_ms, color = '#F2D60C')
+plt.ylim(2,10)
+plt.xlim(.75,None)
+plt.axis('off')
+plt.savefig('kids_game_ratings.png', transparent=True, dpi=200)
+
+plt.clf()
+
+plt.figure(figsize=(8,5))
+plt.plot([np.log(2021-float(g['yearpublished'])) for g in data.values()], [g['stats']['average'] for g in data.values()],'.', markersize=__small_ms, color = '#0097a7')
+#plt.plot([g['stats']['averageweight'] for g in kids_games], [g['stats']['average'] for g in kids_games],'.', color = '#F2D60C')
+
+plt.ylim(2,10)
+#plt.xlim(.75,None)
+plt.axis('off')
+plt.savefig('game_ratings_v_year.png', transparent=True, dpi=200)
+plt.show()
+
+
 X = np.array(list(zip(complexity,age)))
 #X = np.array(complexity).reshape(-1,1)
 y= np.array([g['stats']['average'] for g in data.values()]).reshape(-1,1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 regressor = LinearRegression()
-regressor.fit(X_train, y_train) #training the algorithm
+#regressor.fit(X_train, y_train) #training the algorithm
 
+regressor.fit(X, y) #training the algorithm
+#plt.figure()
+#plt.plot(X[:,0], y-regressor.predict(X), '.')
+#plt.axhline(0, color='k')
+#plt.show()
 def normed_score(game):
     ''' BBG ratings are biased towards complicated games and also
     against older games. Eventually I will use regression to correct for
     the bias.... but for now...'''
 
     return game['stats']['average']-regressor.predict([[game['stats']['averageweight'],np.log(2021-float(game['yearpublished']))]] )[0,0]
+    #return game['stats']['average']-regressor.predict([[game['stats']['averageweight']]]) [0,0]
 
 for game in data.values():
     game['overall_score'] = normed_score(game)
+
+def mape(game):
+    ''' BBG ratings are biased towards complicated games and also
+    against older games. Eventually I will use regression to correct for
+    the bias.... but for now...'''
+
+    return np.abs(game['stats']['average']-regressor.predict([[game['stats']['averageweight'],np.log(2021-float(game['yearpublished']))]] )[0,0])/game['stats']['average']
+    #return game['stats']['average']-regressor.predict([[game['stats']['averageweight']]]) [0,0]
+n = 0
+sum = 0
+for game in data.values():
+    sum += mape(game)
+    n += 1
+print('Mean Absolute percentage Error:', sum/n)
+print('Mean Absolute Error:', metrics.mean_absolute_error(y, regressor.predict(X))/np.average(y))
+print('Mean Squared Error:', metrics.mean_squared_error(y, regressor.predict(X)))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y, regressor.predict(X))))
 
 analyser = SentimentIntensityAnalyzer()
 
@@ -100,7 +173,31 @@ def return_n_best_comments(game, N, search_string):
 
 def return_n_best(N, search_string):
     return filter(lambda x: x[0]>-10, heapq.nlargest(N, map(lambda g: (new_score(g, search_string), g['game_id']), kids_games)))
+plt.clf()
+mem_games = [data[x[1]] for x in return_n_best(500,' memory')]
+plt.plot([g['stats']['averageweight'] for g in data.values()], [g['stats']['average'] for g in data.values()],'.', markersize = __small_ms, color = '#0097a7')
+plt.plot([g['stats']['averageweight'] for g in mem_games], [g['stats']['average'] for g in mem_games ],'.', markersize = __big_ms, color = '#B31269')
 
+
+
+plt.ylim(2,10)
+plt.xlim(.75,None)
+plt.axis('off')
+plt.savefig('mem_game_ratings.png', transparent=True, dpi=200)
+
+plt.clf()
+plt.plot(X[:,0], y-regressor.predict(X) + np.average(y), '.', markersize = __small_ms, color = '#0097a7')
+
+
+
+plt.plot([g['stats']['averageweight'] for g in mem_games], [g['overall_score'] + np.average(y) for g in mem_games ],'.',  markersize = __big_ms,  color = '#B31269')
+#plt.axhline( np.average(y))
+
+
+plt.ylim(2,10)
+plt.xlim(.75,None)
+plt.axis('off')
+plt.savefig('debiased_ratings.png', transparent=True, dpi=200)
 ##### The individual mechanics score
 scores_dict = {
     #'dexterity_score': 'dexterity',
